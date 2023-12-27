fb8e48165b8821033f6fe066e24e92f9bb91fd60
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-12-27 09:56:05 -0300

    commit

---
+++ universiaTour/src/main/java/com/projetoFinal/universiaTour/UniversiaTourApplication.java
@@ -0,0 +1,13 @@
+package com.projetoFinal.universiaTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversiaTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversiaTourApplication.class, args);
+	}
+
+}
---
+++ universiaTour/src/test/java/com/projetoFinal/universiaTour/UniversiaTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoFinal.universiaTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversiaTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
