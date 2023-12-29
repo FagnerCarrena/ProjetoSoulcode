7f89298a1caa7430068ebf08db997150d0c9f6ee
GabrielFrutuoso <mackbiel03@gmail.com>
2023-12-29 15:41:29 -0300

    alterações nas páginas: index, cadastro, perfil

---
+++ universiaTour/src/main/resources/static/JS/Script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal')
+
+const closeModal = ()=> {
+    modal.classList.add('hidden')
+}
+
+const openModal = ()=> {
+    modal.classList.remove('hidden')
+}
+
+// console.log('hekll');
+
+
